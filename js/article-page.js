f607527d22e0d09833f4173ca60be00c0210252e
Edgar <jurirulez@gmail.com>
2022-07-28 15:56:17 +0300

    header shadow

--- js/article-page.js
+++ js/article-page.js
@@ -1,40 +1,9 @@
-const sidebarMenu = document.querySelector(".main-article__sidebar-menu");
-const hideModalMenu = document.querySelector(".icon-close");
-const links = document.querySelectorAll(".main-article__sidebar-item");
-const overlay = document.querySelector(".main-article__overlay");
-
-const buttonShowMenu = document
-  .querySelector(".icon-menu")
-  .addEventListener("click", () => {
-    sidebarMenu.classList.add("show-menu");
-    overlay.classList.add("show-menu");
+$(document).ready(function () {
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 150) {
+      $(".main-article-header").addClass("show-shadow");
+    } else {
+      $(".main-article-header").removeClass("show-shadow");
+    }
   });
-
-function onHandleCloseModal() {
-  hideModalMenu.addEventListener("click", () => {
-    sidebarMenu.classList.remove("show-menu");
-    overlay.classList.remove("show-menu");
-  });
-}
-
-onHandleCloseModal();
-
-function onHandleCloseModalLink() {
-  links.forEach((link) => {
-    link.addEventListener("click", () => {
-      sidebarMenu.classList.remove("show-menu");
-      overlay.classList.remove("show-menu");
-    });
-  });
-}
-
-onHandleCloseModalLink();
-
-function onHandleCloseOverlay() {
-  overlay.addEventListener("click", () => {
-    sidebarMenu.classList.remove("show-menu");
-    overlay.classList.remove("show-menu");
-  });
-}
-
-onHandleCloseOverlay();
+});
